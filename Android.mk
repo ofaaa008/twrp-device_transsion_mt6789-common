5dfb5411bf8880ac508e031d214aa70b5d754204
ofaaa008 <166842264+ofaaa008@users.noreply.github.com>
2024-06-04 00:19:05 +0700

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -6,7 +6,7 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter Infinix-X678B Infinix-X6833B TECNO-CK7n TECNO-LH7n Infinix-X676C,$(TARGET_DEVICE)),)
+ifneq ($(filter Infinix-X678B Infinix-X6833B TECNO-CK7n TECNO-LH7n Infinix-X676C,itel-S666LN,$(TARGET_DEVICE)),)
 
 include $(call all-subdir-makefiles,$(LOCAL_PATH))
 
